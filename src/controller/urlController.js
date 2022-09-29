dfdc6b6d19162509ea2460477d6d6d021333d1b3
RITIK KOHLI <ritikkohli@gmail.com>
2022-09-29 12:45:02 +0530

    again

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -4,16 +4,21 @@ var validUrl = require('valid-url');
   
 
 const createUrl = async function(req,res){
-    if(!req.body) return  res.status(400).send({status:false,message: "Request body can't be empty"})
-    if(!req.body.longUrl || typeof req.body.longUrl !== 'string') return res.status(400).send({status:false,message: "Please provide original url"})
-    if(!validUrl.isUri(req.body.longUrl.trim())) return res.status(400).send({status:false,message: "Please provide a valid url"})
-    let data = req.body
-    let urlCode = shortid.generate().toLowerCase()
-    let shortUrl = `http://localhost:3000/${urlCode}`
-    data.urlCode = urlCode
-    data.shortUrl = shortUrl
-    let saved = await urlModel.create(data)
-    res.status(201).send({status:true,data:saved})
+    try{
+        if(!req.body) return  res.status(400).send({status:false,message: "Request body can't be empty"})
+        if(!req.body.longUrl || typeof req.body.longUrl !== 'string') return res.status(400).send({status:false,message: "Please provide original url"})
+        if(!validUrl.isUri(req.body.longUrl.trim())) return res.status(400).send({status:false,message: "Please provide a valid url"})
+        let data = req.body
+        let urlCode = shortid.generate().toLowerCase()
+        let shortUrl = `http://localhost:3000/${urlCode}`
+        data.urlCode = urlCode
+        data.shortUrl = shortUrl
+        let saved = await urlModel.create(data)
+        res.status(201).send({status:true,data:saved})
+    }
+    catch(err){
+        res.status(500).send(err.message)
+    }
 }
 
 
