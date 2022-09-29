8ffcff4b0409ac8ec8fcbccdd9e23277b2e616c5
Tejush Singh <tejush1998@gmail.com>
2022-09-29 12:51:18 +0530

    readMe

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+var bodyParser = require('body-parser');
+const multer = require('multer')
+const route = require('./routes/route')
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect("mongodb+srv://ritikkohli:eJ9TDANLzfmCixVu@cluster0.gd4mqlp.mongodb.net/group50Database", {
+const mongoose=require('mongoose')
+mongoose.connect("mongodb+srv://book-management:31VMJSSWSjcdoaAO@cluster0.eemzti8.mongodb.net/group48Database?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer().any()) 
 app.use('/', route);
+
 app.use(function(req,res){
     return res.status(404).send({status:false,message:"Path not Found."})
   })
+  
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
 
